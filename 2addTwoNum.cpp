7f6b2b0a30834527a26a517c7467cc2c508b5c50
maywanting <maywanting@gmail.com>
2017-11-05 16:19:54 +0900

    add describtion

--- 2addTwoNum.cpp
+++ 2addTwoNum.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/**
+ * describe:
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+
 using namespace std;
 
 struct ListNode {
@@ -50,6 +56,7 @@ public:
 
         return res.next;
     }
+    acds
 };
 
 int main() {
